22d0d1b3062712eb4b540447742fd05b0ab5a8b3
ryan <ball.ryan.rb@gmail.com>
2021-05-09 19:47:20 -0600

    adds routes for URL and Body parameters (already had Query)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "display_actor" => "actors#display_actor"
 
+  get "query_display_actor" => "actors#display_actor"
+  # displays a single actor by taking a query parameter in a GET web request
+  
+  get "url_display_actor/:id" => "actors#display_actor"
+  # displays a single actor by taking a URL parameter in a GET web request
+  
+  post "body_display_actor" => "actors#display_actor"
+  # displays a single actor by taking a body parameter in a POST web request
+  
   get "display_all_actors" => "actors#display_all_actors"
 
   get "display_movie" => "movies#display_movie"
@@ -11,4 +19,5 @@ Rails.application.routes.draw do
   get "twenty_first_century_movies" => "movies#twenty_first_century_movies"
 
   get "movie_id_title" => "movies#movie_id_title"
+  
 end
