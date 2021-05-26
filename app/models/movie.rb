d315f5774b81c8b55cb60196b325b4ce6c8077eb
ryan <ball.ryan.rb@gmail.com>
2021-05-25 18:28:31 -0600

    adds Genre and MovieGenre model, creates associations, and movie serializer

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,14 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   validates :title, presence: true, uniqueness: true
   validates :year, numericality: { less_than: (Date.today.year + 1) }
   validates :plot, length: { minimum: 50 }
   validates :director, presence: true, length: { in: 4..50 }
+
+  def genre_names
+    genres.pluck(:name)
+  end
 end
